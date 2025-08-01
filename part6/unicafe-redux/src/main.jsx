39f1dc3a3ea3ea31a1c35121cfaca438cc195c81
ivanpo1 <ivan.poppino@gmail.com>
2025-08-01 08:34:43 -0300

    added part6 folder and projects

---
+++ part6/unicafe-redux/src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  // const good = () => {
+  //   store.dispatch({
+  //     type: 'GOOD'
+  //   })
+  // }
+
+  const updateStat = (type) => {
+    console.log(type.toUpperCase())
+    store.dispatch({
+      type: type.toUpperCase()
+    })
+  }
+
+  return (
+    <div>
+      <button onClick={() => updateStat('good')}>good</button>
+      <button onClick={() => updateStat('ok')}>ok</button>
+      <button onClick={() => updateStat('bad')}>bad</button>
+      <button onClick={() => updateStat('zero')}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
