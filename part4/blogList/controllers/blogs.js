568490cb1110a66c9f4d180014f344ac3d9b0cdb
ivanpo1 <ivan.poppino@gmail.com>
2025-08-27 09:10:58 -0300

    added comment functionality for part7

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -1,26 +1,26 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
-const User = require('../models/user')
-const { userExtractor } = require("../utils/middleware")
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+const User = require("../models/user");
+const { userExtractor } = require("../utils/middleware");
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
-  response.json(blogs)
-})
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  response.json(blogs);
+});
 
-blogsRouter.post('/', userExtractor, async (request, response, next) => {
+blogsRouter.post("/", userExtractor, async (request, response, next) => {
   try {
-    const body = request.body
+    const body = request.body;
     if (!body.title || !body.url || !body.author) {
       return response.status(400).json({
-        error: 'Title, Url and Author are required fields'
-      })
+        error: "Title, Url and Author are required fields",
+      });
     }
-    const user = await User.findById(request.user.id)
+    const user = await User.findById(request.user.id);
     if (!user) {
       return response.status(404).json({
-        error: 'User not found'
-      })
+        error: "User not found",
+      });
     }
 
     const blog = new Blog({
@@ -29,56 +29,98 @@ blogsRouter.post('/', userExtractor, async (request, response, next) => {
       url: body.url,
       user: user._id,
       likes: body.likes || 0,
-    })
+    });
 
-    const savedBlog = await blog.save()
+    const savedBlog = await blog.save();
 
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
 
-    const populatedBlog = await Blog.findById(savedBlog._id).populate('user', { username: 1, name: 1 })
-    response.status(201).json(populatedBlog)
-
-  } catch(error) {
-    next(error)
+    const populatedBlog = await Blog.findById(savedBlog._id).populate("user", {
+      username: 1,
+      name: 1,
+    });
+    response.status(201).json(populatedBlog);
+  } catch (error) {
+    next(error);
   }
-})
+});
 
-blogsRouter.delete('/:id', userExtractor, async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
+blogsRouter.delete("/:id", userExtractor, async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
   if (!blog) {
-    return response.status(404).json({ error: 'blog not found' })
+    return response.status(404).json({ error: "blog not found" });
   }
-  const user = await User.findById(request.user.id)
+  const user = await User.findById(request.user.id);
   if (!user) {
-    return response.status(404).json({ error: 'user not found' })
+    return response.status(404).json({ error: "user not found" });
   }
 
   if (blog.user.toString() === user._id.toString()) {
-    await Blog.findByIdAndDelete(request.params.id)
-    return response.status(204).end()
+    await Blog.findByIdAndDelete(request.params.id);
+    return response.status(200).end();
   }
 
-  return response.status(403).json({ error: 'only creator can delete post' })
+  return response.status(403).json({ error: "only creator can delete post" });
+});
 
-})
-
-blogsRouter.put('/:id', userExtractor, async (request, response, next) => {
-  const body = request.body
+blogsRouter.put("/:id", userExtractor, async (request, response, next) => {
+  const body = request.body;
   // const user = await User.findById(request.user.id)
-
+  const start = Date.now();
   try {
-    const blogToUpdate = await Blog.findById(request.params.id)
-    blogToUpdate.likes = body.likes
-    blogToUpdate.author = body.author
-    blogToUpdate.title = body.title
-    blogToUpdate.url = body.url
-    const savedBlog = await blogToUpdate.save()
+    const blogToUpdate = await Blog.findById(request.params.id);
+    blogToUpdate.likes = body.likes;
+    blogToUpdate.author = body.author;
+    blogToUpdate.title = body.title;
+    blogToUpdate.url = body.url;
+    const savedBlog = await blogToUpdate.save();
+    console.log("PUT /blogs/:id duration", Date.now() - start, "ms");
 
-    response.status(200).json(await savedBlog.populate('user', { username: 1, name: 1 }))
+    response
+      .status(200)
+      .json(await savedBlog.populate("user", { username: 1, name: 1 }));
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-module.exports = blogsRouter
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id).populate("user", {
+    username: 1,
+    name: 1,
+  });
+
+  response.json(blog);
+});
+
+blogsRouter.post("/:id/comments", async (request, response, next) => {
+  try {
+    console.log("BACKEND");
+    const blog = await Blog.findById(request.params.id);
+    const body = request.body;
+    console.log("blog", blog);
+    console.log("comment", body.comment);
+
+    if (!blog) {
+      return response.status(404).json({
+        error: "Blog not found",
+      });
+    }
+
+    if (!body.comment) {
+      return response
+        .status(400)
+        .json({ error: "Comment is a required field" });
+    }
+
+    blog.comments = blog.comments.concat(body.comment);
+    const updatedBlog = await blog.save();
+
+    response.status(201).json(updatedBlog);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = blogsRouter;
--- part4/blogList/models/blog.js
+++ part4/blogList/models/blog.js
@@ -1,28 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   author: String,
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: "User",
   },
   url: {
     type: String,
-    required: true
+    required: true,
   },
-  likes: { type: Number, default: 0 }
-})
+  likes: { type: Number, default: 0 },
+  comments: [
+    {
+      type: String,
+    },
+  ],
+});
 
-blogSchema.set('toJSON', {
+blogSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   },
-})
+});
 
-module.exports = mongoose.model('Blog', blogSchema)
+module.exports = mongoose.model("Blog", blogSchema);
